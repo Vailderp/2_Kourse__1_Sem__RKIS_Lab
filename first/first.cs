7097c8af079aa7196db88423db5c5837ffe13cf1
Vailderp <59770659+Vailderp@users.noreply.github.com>
2021-10-25 14:34:29 +0700

    Create first.cs

---
+++ first/first.cs
@@ -0,0 +1,11 @@
+static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя: ");
+            string name = Console.ReadLine();
+            SayHello(name);
+            Console.ReadKey();
+        }
+        static void SayHello(string name)
+        {
+            Console.WriteLine("Здравствуй, " + name);
+        }
